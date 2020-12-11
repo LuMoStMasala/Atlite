bf512e3c38b0fbed17e6c5a00c3b66d23b5b72bc
stefanburbach <75206029+stefanburbach@users.noreply.github.com>
2020-12-11 12:11:00 +0100

    Historical reproduction
    ..is the total of the example from the atlite docs

--- Historical reproduction Germany.py
+++ Historical reproduction Germany.py
@@ -1,44 +1,96 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 10 17:48:15 2020
+Created on Sat Dec  5 13:07:59 2020
 
-@author: Monisha
+@author: Stefan
 """
-
 import atlite
 import xarray as xr
 import pandas as pd
 import scipy.sparse as sp
 import numpy as np
 
-import pgeocode #pip install pgeocode
+import pgeocode
 from collections import OrderedDict
 
-import matplotlib.pyplot as plt  #pip innstall matplotlib
+import matplotlib.pyplot as plt
 
-import seaborn as sns  #pip install seaborn
+
+import seaborn as sns
 sns.set_style('whitegrid')
 
 import requests
+import os
 import zipfile
+
+#%%
+
+def download_file(url, local_filename):
+    # variant of http://stackoverflow.com/a/16696317
+    if not os.path.exists(local_filename):
+        r = requests.get(url, stream=True)
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk:
+                    f.write(chunk)
+    return local_filename
+
+opsd_fn = download_file('https://data.open-power-system-data.org/index.php?package=time_series&version=2019-06-05&action=customDownload&resource=3&filter%5B_contentfilter_cet_cest_timestamp%5D%5Bfrom%5D=2012-01-01&filter%5B_contentfilter_cet_cest_timestamp%5D%5Bto%5D=2013-05-01&filter%5BRegion%5D%5B%5D=DE&filter%5BVariable%5D%5B%5D=solar_generation_actual&filter%5BVariable%5D%5B%5D=wind_generation_actual&downloadCSV=Download+CSV',
+                        'time_series_60min_singleindex_filtered.csv')
+
+opsd = pd.read_csv(opsd_fn, parse_dates=True, index_col=0)
+
+# we later use the (in current version) timezone unaware datetime64
+# to work together with this format, we have to remove the timezone
+# timezone information. We are working with UTC everywhere.
+
+opsd.index = opsd.index.tz_convert(None)
+
+# We are only interested in the 2012 data
+opsd = opsd[("2011" < opsd.index) & (opsd.index < "2013")]
+
+
+eeg_fn = download_file('http://www.energymap.info/download/eeg_anlagenregister_2015.08.utf8.csv.zip',
+                        'eeg_anlagenregister_2015.08.utf8.csv.zip')
+
+with zipfile.ZipFile(eeg_fn, "r") as zip_ref:
+    zip_ref.extract("eeg_anlagenregister_2015.08.utf8.csv")
+    
+#%%Create a cutout from ERA-5
 
 import cartopy.io.shapereader as shpreader
 import geopandas as gpd
-import logging
-import os
-
 shp = shpreader.Reader(shpreader.natural_earth(resolution='10m', category='cultural', name='admin_0_countries'))
 de_record = list(filter(lambda c: c.attributes['ISO_A2'] == 'DE', shp.records()))[0]
 de = gpd.GeoSeries({**de_record.attributes, 'geometry':de_record.geometry})
 x1, y1, x2, y2 = de['geometry'].bounds
 
+#%%
+import logging
+import time
+import subprocess
+
+#get current working directory (cwd)
+import os
 cwd = os.getcwd() #current working directory
-cfp = os.path.realpath(__file__) 
+cfp = os.path.realpath(__file__) #current file path
+
 
 logging.basicConfig(level=logging.INFO)
 
+cutout = atlite.Cutout(name="germany_2012_v01",
+                        cutout_dir="C:\MasalaAtlite\ATLITE\cutouts",
+                        module="era5",
+                        xs=slice(5.5, 15.5),
+                        ys=slice(47.2, 55.1),
+                        years=slice(2012, 2012),
+                        months=slice(1,12)
+                        )
+#%%
+cutout.prepare()
 
-cutout = import(C:\MasalaAtlite\ATLITE\cutouts\germany_2012_v01)
+#%% Generate capacity layout
+
 def capacity_layout(cutout, typ, cap_range=None, until=None):
     """Aggregate selected capacities to the cutouts grid into a capacity layout.
 
@@ -127,9 +179,12 @@ def capacity_layout(cutout, typ, cap_range=None, until=None):
 
     return layout
 
+#%%
 
 
 solar_layout = capacity_layout(cutout, 'Solarstrom',cap_range= None, until="2012")
+
+#%%
 
 solar_layout.plot(cmap="inferno_r", size=8, aspect=1)
 plt.title("Installed PV in Germany until 2012")
